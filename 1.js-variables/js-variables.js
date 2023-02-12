2e603897b79a018eaddc857647380640a6386aaa
lemusosa <alvinandwealth@gmail.com>
2023-02-12 16:32:32 +0200

    complete variables lecture

--- 1.js-variables/js-variables.js
+++ 1.js-variables/js-variables.js
@@ -1,12 +1,19 @@
 function addition(_x, _y) {
-    // can use the let, var and const keyword to declare variables
-    // const is a variable that cannot change - use UPPERCASE for const name
-    // ... just a convention to make code readable
-    // var is not used in modern day javascript
-    // use const where you expect the variable not to change
-    // then use let because it is blocked scoped
-    // let and const makes your code less error-prone
-    // and if there is a bug - then causality anaylsis is more traceable
+    /** can use the let, var and const keyword to declare variables
+     * const is a variable that cannot change - use UPPERCASE for const name
+     * just a convention to make code readable
+     * var is not used in modern day javascript
+     * use const where you expect the variable not to change
+     * then use let because it is blocked scoped
+     * let and const makes your code less error-prone
+     * and if there is a bug - then causality anaylsis is more traceable
+     * the variable dat types here in this scetion is mainly integers (whole numbers) 
+     * */ 
+   
+    
+    
+
+    
     let x = _x;
     let y = _y;
     let z = x + y;
@@ -16,6 +23,23 @@ function addition(_x, _y) {
 function subtraction(_x, _y) {
     let x = _x;
     let y = _y;
-    let a = y - x;
+    let a = x - y;
     return a
+}
+
+function strings() {
+    // strings are used for alphanumeric characters that are not necessarily
+    // ... evaluated as numbers. They a re string s- you can strings for a name,
+    // ... sentence, name of a place etc.
+    // strings are wrapped in double quotes or even signle quotes. Adopt to one
+    // convention in your code base 
+    let myName = "Lemuel  S S";
+
+    /** concatenation is when you combining things together instead of adding them
+     * you use the + operator to concatenate.
+     * Concatenation sometime enforces type coercion when two different dtat types: 
+     * string and integer
+     * 
+     */
+    return myName
 }
